82ed28c0a9fb996a0abaaa397af254a8bc8a9536
ximanyd <155841725+ximanyd@users.noreply.github.com>
2025-07-23 13:30:41 -0700

    Add root layout with theme provider

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
